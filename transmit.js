f675b4364abd55eeaddb582428612fb4bb6c482f
dneykov <metheorita@gmail.com>
2021-06-21 02:32:03 +0200

    Add history and fix visual bug on small screens

--- transmit.js
+++ transmit.js
@@ -1,14 +1,14 @@
 function transmit(textfield) {
         var request = new XMLHttpRequest();
         var params = JSON.stringify({"exp":textfield});
-        request.open('POST', 'http://pass.anticitizen.space/compute', true);
+        request.open('POST', 'http://localhost:8080/compute', true);
         request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
         request.send(params);
 
         request.onload = function() {
-            document.querySelector('#result').innerHTML = request.responseText;
-            console.log(JSON.parse(request.responseText).result);
-            console.log(request.responseText);
-            result.innerHTML = JSON.parse(request.responseText).result;
+            document.querySelector('#history').innerHTML += "<br>" + document.querySelector('#result').innerHTML + " = ";
+            var result = JSON.parse(request.responseText).result;
+            document.querySelector('#result').innerHTML = result;
+            document.querySelector('#history').innerHTML += result;
         }
     }
